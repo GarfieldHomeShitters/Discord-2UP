577068864b213b02efefec9f66e9bee98c1acb4a
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-30 21:32:01 +0100

    Create meaningful types for API

--- 2UP Finder/types.go
+++ 2UP Finder/types.go
@@ -22,27 +22,37 @@ type Lay struct {
 }
 
 type Query struct {
-	Bookmakers       []string  `json:"bookmaker"`
-	Exchanges        []string  `json:"exchange"`
-	MinRating        *string   `json:"minRating"`
-	MaxRating        *string   `json:"maxRating"`
-	TimeframeStart   *string   `json:"timeframeStart"`
-	TimeframeEnd     *string   `json:"timeframeEnd"`
-	EventNameFilter  *string   `json:"searchByEventName"`
-	Limit            int       `json:"limit"`
-	Cap              int       `json:"cap"`
-	LastUpdate       int       `json:"updatedWithinSeconds"`
-	ExcludeDraws     bool      `json:"excludeDraw"`
-	MinimumLiquidity *string   `json:"minLiquidity"`
-	RatingType       *string   `json:"ratingType"`
-	MinOdds          *float64  `json:"minOdds"`
-	MaxOdds          *float64  `json:"maxOdds"`
-	MarketGroups     []string  `json:"permittedMarketGroups"`
-	Sports           []string  `json:"permittedSports"`
-	Skip             *int      `json:"skip"`
-	EventGroups      []*string `json:"permittedEventGroups"`
-	CommissionRates  []struct {
-		Exchange string `json:"exchange"`
-		Rate     int    `json:"rate"`
-	} `json:"commissionRates"`
+	Bookmakers       []string        `json:"bookmaker"`
+	Exchanges        []string        `json:"exchange"`
+	MinRating        *string         `json:"minRating"`
+	MaxRating        *string         `json:"maxRating"`
+	TimeframeStart   *string         `json:"timeframeStart"`
+	TimeframeEnd     *string         `json:"timeframeEnd"`
+	EventNameFilter  *string         `json:"searchByEventName"`
+	Limit            int             `json:"limit"`
+	Cap              int             `json:"cap"`
+	LastUpdate       int             `json:"updatedWithinSeconds"`
+	ExcludeDraws     bool            `json:"excludeDraw"`
+	MinimumLiquidity *string         `json:"minLiquidity"`
+	RatingType       string          `json:"ratingType"`
+	MinOdds          *string         `json:"minOdds"`
+	MaxOdds          *string         `json:"maxOdds"`
+	MarketGroups     []string        `json:"permittedMarketGroups"`
+	Sports           []string        `json:"permittedSports"`
+	Skip             *int            `json:"skip"`
+	EventGroups      []*string       `json:"permittedEventGroups"`
+	CommissionRates  []CommisionRate `json:"commissionRates"`
 }
+
+type CommisionRate struct {
+	Exchange string `json:"exchange"`
+	Rate     int    `json:"rate"`
+}
+
+type FullQuery struct {
+	Query         string `json:"Query"`
+	Variables     Query  `json:"Variables"`
+	OperationName string `json:"OperationName"`
+}
+
+type QueryType int
