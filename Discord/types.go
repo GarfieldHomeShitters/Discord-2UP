09f93dd5d3542a92391c71cbc6ff7239cea4683c
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-30 05:54:12 +0100

    Add types necessary for logging/notifying

---
+++ Discord/types.go
@@ -0,0 +1,25 @@
+package webhook_manager
+
+type Message struct {
+	Content *string `json:"content,omitempty"`
+	Embeds *[]Embed `json:"embeds,omitempty"`
+	AllowedMentions *AllowedMentions `json:"allowed_mentions,omitempty"`
+}
+
+type Embed struct {
+	Title *string `json:"title,omitempty"`
+	Color  *string `json:"color,omitempty"`
+	Fields *[]Field `json:"fields,omitempty"`
+}
+
+type AllowedMentions struct {
+	Parse *[]string `json:"parse,omitempty"`
+	Users *[]string `json:"users,omitempty"`
+	Roles *[]string `json:"roles,omitempty"`
+}
+
+type Field struct {
+	Name *string `json:"name,omitempty"`
+	Value *string `json:"value,omitempty"`
+	Inline *bool   `json:"inline,omitempty"`
+}
