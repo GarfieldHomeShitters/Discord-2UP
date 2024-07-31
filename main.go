4e4b708adad66667e91c04e6540fce83d77a6025
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-31 01:02:55 +0100

    Fix miscapitalized variables and update structure correctly + update git ignore

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"Adam/discord-twoup/MatchFinder"
-	"fmt"
 )
 
 func main() {
 	qt := MatchFinder.TwoUp
 
-	qR := MatchFinder.Find(qt)
-	if qR != nil {
-		fmt.Println("Executed:", qR)
-	} else {
-		fmt.Println("Failed!")
-	}
+	MatchFinder.Find(qt)
+
 }
