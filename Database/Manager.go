71eace7f73d06a7252fa766deea8688293578fc7
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-08-04 03:21:46 +0100

    Create (untested) CRUD functionality for generic DB and oracle nosqldb

---
+++ Database/Manager.go
@@ -0,0 +1,175 @@
+package Database
+
+import (
+	"fmt"
+	"github.com/oracle/nosql-go-sdk/nosqldb"
+	"github.com/oracle/nosql-go-sdk/nosqldb/auth/iam"
+	"github.com/oracle/nosql-go-sdk/nosqldb/common"
+	"github.com/oracle/nosql-go-sdk/nosqldb/types"
+)
+
+type Database interface {
+	Connect() error
+	Close() error
+	SelectTable(string) error
+	Get(string) (error, string)
+	Put(string, []byte) error
+	Delete(string, string) error
+}
+
+type Handler struct {
+	db Database
+}
+
+type OracleDB struct {
+	Connected  bool
+	ConfigPath string
+	Region     common.Region
+	Client     *nosqldb.Client
+	Table      *string
+}
+
+func (db *OracleDB) Connect() error {
+	AuthProv, err := iam.NewSignatureProviderFromFile(db.ConfigPath, "", "", "")
+	if err != nil {
+		return NewDatabaseError("Connection:", err.Error())
+	}
+
+	cfg := nosqldb.Config{
+		Region:                db.Region,
+		AuthorizationProvider: AuthProv,
+	}
+
+	client, err := nosqldb.NewClient(cfg)
+	if err != nil {
+		return NewDatabaseError("Client Creation:", err.Error())
+	}
+
+	db.Client = client
+	return nil
+}
+
+func (db *OracleDB) Close() error {
+	if db.Connected == false {
+		return NewDatabaseError("Client Closing:", "No connection is open")
+	}
+
+	if db.Client == nil {
+		return NewDatabaseError("Client Closing:", "No client to close")
+	}
+
+	err := db.Client.Close()
+	if err != nil {
+		return NewDatabaseError("Client Closing:", err.Error())
+	}
+	db.Connected = false
+
+	return nil
+}
+
+func (db *OracleDB) SelectTable(name string) error {
+	db.Table = &name
+	return nil
+}
+
+func (db *OracleDB) Get(Field string, Value string) (*string, error) {
+	callTrace := fmt.Sprintf("\n\tTable:\t%s\n\tGet:\t%s:\t%s", *db.Table, Field, Value)
+	if !db.Connected {
+		return nil, NewDatabaseError(callTrace, "Client not connected")
+	}
+
+	if db.Table == nil {
+		return nil, NewDatabaseError(callTrace, "Table is nil")
+	}
+
+	keyMap := &types.MapValue{}
+	keyMap.Put(Field, Value)
+	req := &nosqldb.GetRequest{
+		TableName: *db.Table,
+		Key:       keyMap,
+	}
+	res, err := db.Client.Get(req)
+	if err != nil {
+		return nil, NewDatabaseError(callTrace, err.Error())
+	}
+	if !res.RowExists() {
+		return nil, NewDatabaseError(callTrace, "No row exists")
+	}
+	json := res.ValueAsJSON()
+	return &json, nil
+}
+
+func (db *OracleDB) Put(record interface{}) error {
+	callTrace := fmt.Sprintf("\n\tTable:\t%s\n\tPut:\t%+v", *db.Table, record)
+	if !db.Connected {
+		return NewDatabaseError(callTrace, "Client not connected")
+	}
+
+	if db.Table == nil {
+		return NewDatabaseError(callTrace, "Table is nil")
+	}
+
+	if record == nil {
+		return NewDatabaseError(callTrace, "Record is empty")
+	}
+
+	putRq := &nosqldb.PutRequest{
+		TableName:   *db.Table,
+		StructValue: record,
+		ExactMatch:  true,
+	}
+	res, err := db.Client.Put(putRq)
+	if err != nil {
+		return NewDatabaseError(callTrace, err.Error())
+	}
+
+	if !res.Success() {
+		why := fmt.Sprintf("Put failed: %s", res.String())
+		return NewDatabaseError(callTrace, why)
+	}
+
+	return nil
+
+}
+
+func (db *OracleDB) Delete(key string, val string) error {
+	callTrace := fmt.Sprintf("\n\tTable:\t%s\n\tDelete:\t%s :\t%s", *db.Table, key, val)
+	if !db.Connected {
+		return NewDatabaseError(callTrace, "Client not connected")
+	}
+	if db.Table == nil {
+		return NewDatabaseError(callTrace, "Table is nil")
+	}
+
+	deleteMap := &types.MapValue{}
+	deleteMap.Put(key, val)
+	deleteRq := &nosqldb.DeleteRequest{
+		TableName: *db.Table,
+		Key:       deleteMap,
+	}
+
+	res, err := db.Client.Delete(deleteRq)
+	if err != nil {
+		return NewDatabaseError(callTrace, err.Error())
+	}
+
+	if !res.Success {
+		why := fmt.Sprintf("Delete failed: %s", res.String())
+		return NewDatabaseError(callTrace, why)
+	}
+
+	return nil
+}
+
+type DbError struct {
+	message string
+}
+
+func (e *DbError) Error() string {
+	return fmt.Sprintf("[DatabaseError] %s", e.message)
+}
+
+func NewDatabaseError(where string, what string) *DbError {
+	msg := fmt.Sprintf("\t%s \n\t%s", where, what)
+	return &DbError{message: msg}
+}
