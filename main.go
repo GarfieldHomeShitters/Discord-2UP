b9209df809ecf40a5a34a80aa161ec57d76f50ab
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-08-05 00:29:19 +0100

    Handle db properly + make use of typed errors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Adam/discord-twoup/Database"
 	"Adam/discord-twoup/Handlers"
 	"Adam/discord-twoup/MatchFinder"
 	"context"
@@ -15,6 +16,81 @@ type NotificationHandler struct {
 	Handlers.ErrorHandler
 }
 
+type DatabaseHandler struct {
+	Database.Database
+}
+
+type DbItem struct {
+	ID             string `json:"id"`
+	EventStartDate string `json:"EventStartDate"`
+}
+
+func main() {
+	Notifier := CreateNotificationHandler()
+	Db := CreateDatabaseHandler()
+
+	dbError := Db.Connect()
+	if dbError != nil {
+		nErr := Notifier.LogTypedError(dbError)
+		if nErr != nil {
+			str := fmt.Sprintf("Error logging error: %v\n", dbError)
+			log.Fatal(str)
+		}
+		return
+	}
+	defer Db.Close()
+
+	typedErr := Db.SelectTable("PreviouslyNotified")
+	if typedErr != nil {
+		Notifier.LogTypedError(typedErr)
+		panic(typedErr.Error())
+	}
+
+	qR, err := MatchFinder.Find(MatchFinder.TwoUp)
+	if err != nil {
+		notificationErr := Notifier.LogError(err)
+		if notificationErr != nil {
+			errStr := fmt.Sprintf("Error logging error: %v\n", err)
+			log.Panic(errStr)
+		}
+		return
+	}
+	// TODO: Later -> handle no new matches and softly inform user -> log in console instead of via discord.
+	getErr, newMatches := filterMatches(qR, Db)
+	if getErr != nil {
+		notificationErr := Notifier.LogTypedError(getErr)
+		if notificationErr != nil {
+			errStr := fmt.Sprintf("Error logging error: %v\n", err)
+			log.Panic(errStr)
+		}
+		return
+	}
+
+	err = Notifier.NotifyUser(context.Background(), &newMatches)
+	if err != nil && err.Error() != "no_matches" {
+		fmt.Println("Failed Webhook:", err)
+		notificationErr := Notifier.LogError(err)
+		if notificationErr != nil {
+			errStr := fmt.Sprintf("Error logging error: %v\n", err)
+			log.Panic(errStr)
+		}
+		return
+	}
+
+	for _, v := range newMatches {
+		item := NewDatabaseItem(v)
+		typedErr = Db.Put(item)
+		if typedErr != nil {
+			notificationErr := Notifier.LogTypedError(typedErr)
+			if notificationErr != nil {
+				errStr := fmt.Sprintf("Error logging error: %v\n", typedErr)
+				log.Fatal(errStr)
+			}
+			return
+		}
+	}
+}
+
 func CreateNotificationHandler() *NotificationHandler {
 	UserIDs := []string{"261512678269255681"}
 	ping := Handlers.NewPingType(nil, UserIDs, nil)
@@ -26,27 +102,36 @@ func CreateNotificationHandler() *NotificationHandler {
 	}
 }
 
-func main() {
-	Notifier := CreateNotificationHandler()
-
-	qt := MatchFinder.TwoUp
-	qR, err := MatchFinder.Find(qt)
-	if err != nil {
-		notificationErr := Notifier.LogError(err)
-		if notificationErr != nil {
-			errStr := fmt.Sprintf("Error logging error: %v\n", err)
-			log.Println(errStr)
-			return
-		}
+func CreateDatabaseHandler() *DatabaseHandler {
+	path := "C:\\Users\\Adam\\.oci\\config.ini"
+	db := Database.NewOracleConnection(path, "uk-london-1")
+	return &DatabaseHandler{
+		Database: db,
 	}
-	err = Notifier.NotifyUser(context.Background(), &qR)
-	if err != nil {
-		fmt.Println("Failed Webhook:", err)
-		notificationErr := Notifier.LogError(err)
-		if notificationErr != nil {
-			errStr := fmt.Sprintf("Error logging error: %v\n", err)
-			log.Println(errStr)
-			return
+}
+
+func filterMatches(matches []MatchFinder.Match, Db *DatabaseHandler) (*Database.DataError, []MatchFinder.Match) {
+	var newMatches []MatchFinder.Match
+	for i, v := range matches {
+		getErr, _ := Db.Get("id", v.ID)
+		if getErr == nil {
+			continue
 		}
+
+		if getErr.ErrorType() == "No Row" {
+			newMatches = append(newMatches, matches[i])
+			continue
+		}
+
+		return getErr, nil
+	}
+
+	return nil, newMatches
+}
+
+func NewDatabaseItem(match MatchFinder.Match) *DbItem {
+	return &DbItem{
+		ID:             match.ID,
+		EventStartDate: match.StartDate,
 	}
 }
